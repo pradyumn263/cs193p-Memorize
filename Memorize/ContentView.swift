d04fde432831cb2e46559e589acef6c9cc4c2a2a
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-09-30 15:41:09 +0530

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Pradyumn Shukla on 30/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pradyumn Shukla on 30/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
